b09e0732fc2a74e1dc50984b0dfb1986e3aeebfe
Julian Q Zhou <julian.q.zhou@gmail.com>
2018-02-26 22:53:25 -0500

    working prototype
    quiz (crude) on genders of nouns only

---
+++ french.py
@@ -0,0 +1,251 @@
+import csv
+from random import sample
+
+# meta parameters
+QUIZ_SIZE = 5
+CSV_PATH = "/Users/jkewz/Desktop/fr/"
+CSV_FILENAME = "mots.csv"
+
+CSV_COL_FREQ = "frequency_rank"
+CSV_COL_NA = "noun_article"
+CSV_COL_WORD = "word"
+CSV_COL_POS = "part_of_speech"
+CSV_COL_VAR = "variation"
+CSV_COL_MEAN = "meaning"
+CSV_COL_PHR = "phrases"
+CSV_COL_REL = "related"
+CSV_COL_REG = "register"
+
+LEGAL_NOUN_POS = ("nm", "nf", "nm/nf", "nmpl", "nfpl", "nm/nf pl", "nmi", "nfi")
+LEGAL_GENDER_INPUTS = ("m", "f", "mf", "mpl", "fpl", "mfpl", "mi", "fi")
+
+# print info about expected input
+def printInputInfo():
+
+	print("Type in any order.")
+	print("Use ';' without space for separation.")
+	print("'{0}' for {1}, '{2}' for {3}, '{4}' for {5}".format(
+		LEGAL_GENDER_INPUTS[0], LEGAL_NOUN_POS[0], 
+		LEGAL_GENDER_INPUTS[1], LEGAL_NOUN_POS[1], 
+		LEGAL_GENDER_INPUTS[2], LEGAL_NOUN_POS[2]))
+	print("'{0}' for {1}, '{2}' for {3}, '{4}' for {5}".format(
+		LEGAL_GENDER_INPUTS[3], LEGAL_NOUN_POS[3], 
+		LEGAL_GENDER_INPUTS[4], LEGAL_NOUN_POS[4], 
+		LEGAL_GENDER_INPUTS[5], LEGAL_NOUN_POS[5]))
+	print("'{0}' for {1}, '{2}' for {3}".format(
+		LEGAL_GENDER_INPUTS[6], LEGAL_NOUN_POS[6], 
+		LEGAL_GENDER_INPUTS[7], LEGAL_NOUN_POS[7]))
+	#print("'m' for nm, 'f' for nf, 'mf' for nm/nf")
+	#print("'mpl' for nmpl, 'fpl' for nfpl, 'mfpl' for nm/nf pl")
+	print("E.g.: 'f', 'm;f', 'f;m', 'mf;f;m'")
+
+
+# capture input gender & process into a list
+def getGenderFromKeyboard():
+
+	print("Your input:")
+	allGendersStr = input()
+	
+	allGendersStr = allGendersStr.lower()
+	allGendersList = allGendersStr.split(sep=";")
+
+	isLegal = [gender in LEGAL_GENDER_INPUTS for gender in allGendersList]
+
+	if sum(isLegal)==len(allGendersList):
+		return allGendersList
+	else:
+		print("Incorrect input format.")
+		printInputInfo()
+		return getGenderFromKeyboard()
+
+
+# assess input gender (list) by comparing with truth (list)
+def assessGenderInput(genderInput, genderTruth):
+
+	genderInput.sort()
+	genderTruth.sort()
+
+	lenInput = len(genderInput)
+	lenTruth = len(genderTruth)
+
+	if genderInput == genderTruth:
+		return True
+	else:
+		if lenInput > lenTruth:
+			print("Too many! Expecting {0}; received {1}.".format(lenTruth, lenInput))
+		else:
+			counter = 0
+			for g in genderInput:
+				if g in genderTruth:
+					counter += 1
+			print("{0} out of {1} correct.".format(counter, lenTruth))
+		return False
+
+# format parsed true gender (list from parseNounGender)
+# for printing as correct answer
+def formatAnswer(genderTruth):
+
+	genderTruth.sort()
+	
+	answer = ""
+	for gender in genderTruth:
+		if len(answer)==0:
+			answer = gender
+		else:
+			answer = answer + ";" + gender
+
+	return answer
+
+# given noun and its true gender(s) in a list
+# solicit and assess user input of gender(s)
+def genderQuizSingleWord(quizWord, genderTruth):
+
+	# set max number of failures allowed
+	remainingTrials = 3
+
+	# print word
+	print("* * * * * * * * * * * * * * * * * *")
+	print("Quiz word: " + quizWord)
+
+	while remainingTrials > 0:
+		remainingTrials -=1
+		# solicit input
+		currentInput = getGenderFromKeyboard()
+		# assess input
+		currentAssess = assessGenderInput(currentInput, genderTruth)
+		if currentAssess:
+			print("Très bien!")
+			return
+		else:
+			if remainingTrials>0:
+				print("{0} trials left.".format(remainingTrials))
+			else:
+				# reveal correct answer
+				correctAnswer = formatAnswer(genderTruth)
+				print("Correct answer: {0}".format(correctAnswer))
+
+# parse through parts of speech of a noun to produce a list of gender(s)
+# if input contains irregular format, a msg will be printed and None returned
+def parseNounGender(pos, verbose=False):
+
+	pos = pos.lower()
+
+	# split by semi-colon
+	posList = pos.split("; ")
+
+    # keep only pos starting with "n" and of length >1
+    # "n" (e.g. Londres), for which gender info is unavail., is excluded
+	posList = [item for item in posList if item[0]=="n" and len(item)>1]
+
+	# check that posList is not empty 
+	# will be empty for non-nouns or nouns without gender info (e.g. Londres, n)
+	if len(posList)==0:
+		if verbose: print("No noun POS with gender. None returned.")
+		return
+
+	# check legality of posList against LEGAL_NOUN_POS
+	for item in posList:
+		if item not in LEGAL_NOUN_POS:
+			print("Noun POS contains illegal item ({0}). None returned.".format(item))
+			return
+
+	# parse
+	# nm       -> m
+	# nf       -> f
+	# nm/nf    -> mf
+	# nmpl     -> mpl
+	# nfpl     -> fpl
+	# nm/nf pl -> mfpl
+	genderList = []
+	for item in posList:
+		if item=="nm/nf":
+			genderList.extend(["mf"])
+		elif item=="nm/nf pl":
+			genderList.extend(["mfpl"])
+		else:
+			genderList.extend([item[1:]])
+
+	#print(genderList)
+	return genderList
+
+# given a list of words and their corresponding POS
+# each word is a string; each POS is a string too
+# run gender quiz for nouns for which gender info is available in their POS
+def genderQuizWordList(wordList, posList):
+
+	# check lens match
+	if len(wordList)!=len(posList):
+		print("Lengths of wordList and posList don't match. Something's wrong. Exiting.")
+		return
+
+	for i in range(len(wordList)):
+		pos = parseNounGender(posList[i], verbose=False)
+		#print(type(pos))
+		if pos is not None:
+			genderQuizSingleWord(wordList[i], pos)
+
+# given a csv, generate:
+# e.g. a list of words; each word is a string (e.g. "courageux")
+# e.g. a list of POS corresponding to the words; each POS is a string (e.g. "adj; nm")
+# given a csv, generate:
+# a list of words; each word is a string (e.g. "courageux")
+# a list of POS corresponding to the words; each POS is a string (e.g. "adj; nm")
+def getWordPOSfromCSV(csvname):
+    
+    with open(csvname, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        wordPosList = [ [row[CSV_COL_WORD], row[CSV_COL_POS]] for row in reader ]
+        wordList = [wordPos[0] for wordPos in wordPosList]
+        posList = [wordPos[1] for wordPos in wordPosList]
+    
+    return wordList, posList
+
+# main wrapper function
+# given a csv file, run noun gender quiz through its words
+# if size is specified as an integer (must be <= # words), do random sampling
+def genderQuizMain(csvname, size=None):
+
+	# get wordList and posList
+	wordList, posList = getWordPOSfromCSV(csvname)
+	
+	# random sampling
+	lenList = len(wordList)
+
+	if size is not None:
+		try:
+			idx = sample(list(range(lenList)), k=size)
+			wordList = [wordList[i] for i in idx]
+			posList = [posList[i] for i in idx]
+		except:
+			print("size must be >0 & <= {0}".format(lenList))
+			return
+
+	# run quiz through list
+	printInputInfo()
+	genderQuizWordList(wordList, posList)
+
+
+# run
+# number of words actually on the quiz could be smaller than size
+# this is because non-nouns and nouns with no gender info are omitted
+#genderQuizMain(CSV_PATH+CSV_FILENAME, QUIZ_SIZE)
+genderQuizMain(CSV_FILENAME, QUIZ_SIZE)
+
+# for testing
+
+#printInputInfo()
+#getGenderFromKeyboard()
+#assessGenderInput(["a","c"], ["a","c", "b"])
+#genderQuizSingleWord("tour", ["m","f"])
+#parseNounGender("nm/nf; nmpl; nf; nm; nm/nf pl; nfpl")
+#parseNounGender("n; adj")
+#wL = ["tour", "garçon", "police", "gens", "courageux", "Londres"]
+#pL = ["nm; nf", "nm", "nf", "nmpl", "adj", "n"]
+#genderQuizWordList(wL, pL)
+
+#wL, pL = getWordPOSfromCSV("mots.csv")
+#print(wL[0:5])
+#print(pL[0:5])
+#genderQuizWordList(wL[0:3], pL[0:4])
+
+#print(formatAnswer(["m","f", "mf", "mpl", "fpl", "mfpl"]))
